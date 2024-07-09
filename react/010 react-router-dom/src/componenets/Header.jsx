f4647ecd1e00fe55922ce95b81f8766187ec9cd6
sultanBytes <sultankhand11@gmail.com>
2024-07-09 16:11:04 +0530

    mistry

--- react/010 react-router-dom/src/componenets/Header.jsx
+++ react/010 react-router-dom/src/componenets/Header.jsx
@@ -4,13 +4,15 @@ import { Link, NavLink } from 'react-router-dom'
 const Header = () => {
   return (
     <>
-    <Link to='/' className='mx-5 py-5'>Home</Link>
-    <Link to='/faq'>Faq</Link>
+    {/* <Link to='/' className='mx-5 py-5'>Home</Link>
+    <Link to='/faq'>Faq</Link> */}
 
-    {/* <NavLink to='/' className='mx-'>Home</NavLink> */}
-    {/* <NavLink to='/faq'>Faq</NavLink> */}
+    <NavLink to='/' className={({isActive})=>`mx-5 ${isActive ? 'text-danger' : 'text-success'}` }>Home</NavLink>
+    <NavLink to='/faq' className={({isActive})=>`mx-5 ${isActive ? 'text-danger' : 'text-success'}` }> Faq</NavLink>
     </>
   )
 }
+
+//isActive activeClass activeStyle
 
 export default Header
---
+++ react/011 mistry.store/src/components/common-components/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+//images
+import logo from './../../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiPhoneCall } from 'react-icons/fi';
+import { FaLanguage } from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar expand="lg" className="">
+                <Container fluid>
+                    <Navbar.Brand href="#" className='logo'>
+                        <Link to='/'>
+                            <img src={logo} alt="logo" className="logo" />
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0 navbar-links"
+                            
+                            navbarScroll
+                        >
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/pdf-catelog">PDF Catelog</Nav.Link>
+                            <Nav.Link href="#">Collections By Mistry.Store</Nav.Link>
+                            <Nav.Link href="/pdf-catelog">About Us</Nav.Link>
+                            <Nav.Link href="/pdf-catelog">Contact Us</Nav.Link>
+                            <Nav.Link href="/pdf-catelog">Blogs</Nav.Link>
+                        </Nav>
+                        <div className='d-flex align-items-center'>
+                            <Nav.Link href="/pdf-catelog">
+                            <span className='navbar-icons'>
+                                <FaLanguage/>
+                            </span>
+                            </Nav.Link>
+                            <Nav.Link href="/pdf-catelog">
+                            <span className='navbar-icons'>
+                                <FiPhoneCall/>
+                            </span>
+                            </Nav.Link>
+                            <Nav.Link href="/pdf-catelog">
+                            <button className='login-btn'>Login</button>
+                            </Nav.Link>
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default Header
